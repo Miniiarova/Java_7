c62952da3ad1be3b4d7208336e59c6992884ebd0
MarinaM <marina.miniiarova@gmail.com>
2024-06-18 21:03:49 +0700

    создала тест

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Assertions;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxAtTheBeging() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
@@ -16,5 +16,17 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxAtTheEnd() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 43};
+        long expected = 43;
+
+        long actual = service.findMax(incomesInBillions);
+
+
     }
 }
